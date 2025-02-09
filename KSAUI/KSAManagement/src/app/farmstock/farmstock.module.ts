ad4fea043394f34759e86d9bab02b1b00d3ad618
unknown <appaarao22@gmail.com>
2025-02-09 17:07:41 +0530

    UI Changes with farmcomponent and routing

---
+++ KSAUI/KSAManagement/src/app/farmstock/farmstock.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FarmstockRoutingModule } from './farmstock-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FarmstockRoutingModule
+  ]
+})
+export class FarmstockModule { }
---
+++ KSAUI/KSAManagement/src/app/farmstock/farmstock/farmstock.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-farmstock',
+  imports: [],
+  templateUrl: './farmstock.component.html',
+  styleUrl: './farmstock.component.css'
+})
+export class FarmstockComponent {
+
+}
